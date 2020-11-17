ec41ad9ba0c62912519edf6e72c1674c3f2ba407
Luciano S. de Arruda <71555478+lucianoecp@users.noreply.github.com>
2020-11-16 22:58:21 -0300

    adicionada pasta de programação basica

---
+++ ProgramacaoBasica/ProgBasicaEx04.quest1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ProgBasicaEx04.quest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano;
+
+            Console.Write("Informe o ano (a partir de 1001): ");
+            ano = int.Parse(Console.ReadLine());
+
+            if(ano < 1001) 
+            {
+                Console.WriteLine("[ERRO!] - Entrada inválida!\n");
+                Console.WriteLine("Informe um ano válido a partir do ano 1001");
+            }
+            else if ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0)))
+            {
+                Console.WriteLine("\nO ano "+ano+" é bissexto\n");
+            }
+            else
+            {
+                Console.WriteLine("\nO ano " + ano + " não é bissexto\n");
+            }
+        }
+    }
+}
